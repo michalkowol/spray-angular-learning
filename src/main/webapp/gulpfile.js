b8851d9ffa31ddc9941d77970bfa70e2b0356fc6
Michal Kowol <michal.kowol@vimn.com>
2014-07-11 12:50:41 +0200

    NT Minor changes

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -92,7 +92,7 @@
     gulp.task('usemin', function () {
         return gulp.src('./dist/**/*.html')
             .pipe(usemin({
-                css: [minifycss(), 'concat', prefix('last 2 versions')],
+                css: [minifycss(), 'concat', prefix('last 2 versions'), rev()],
                 js: [ngmin({dynamic: true}), uglify(), rev()]
             }))
             .pipe(gulp.dest('./dist/'));
