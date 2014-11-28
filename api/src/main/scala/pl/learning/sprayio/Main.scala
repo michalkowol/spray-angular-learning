e1060be0e5e6771b5ab87dff8ed0db72b124bcb5
kmichal <michal.kowol@gmail.com>
2014-11-28 18:37:35 +0100

    NT Added SSL support

--- api/src/main/scala/pl/learning/sprayio/Main.scala
+++ api/src/main/scala/pl/learning/sprayio/Main.scala
@@ -3,9 +3,13 @@ package pl.learning.sprayio
 import akka.actor.{ Props, ActorSystem }
 import akka.io.IO
 import spray.can.Http
+import spray.can.server.ServerSettings
 
-object Main extends App {
+object Main extends App with CustomSslConfiguration {
   implicit val system = ActorSystem("api-system")
   val api = system.actorOf(Props[Api], "api")
+  val settings = ServerSettings(system)
+
   IO(Http) ! Http.Bind(api, "0.0.0.0", port = 8080)
+  IO(Http) ! Http.Bind(api, "0.0.0.0", port = 8443, settings = Some(settings.copy(sslEncryption = true)))
 }
