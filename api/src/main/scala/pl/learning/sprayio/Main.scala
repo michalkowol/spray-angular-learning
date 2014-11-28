d7318211cfd4d99fe6db72ce0f909eb2907c2340
kmichal <michal.kowol@gmail.com>
2014-11-28 19:04:09 +0100

    NT Removed SSL support

--- api/src/main/scala/pl/learning/sprayio/Main.scala
+++ api/src/main/scala/pl/learning/sprayio/Main.scala
@@ -5,11 +5,9 @@ import akka.io.IO
 import spray.can.Http
 import spray.can.server.ServerSettings
 
-object Main extends App with CustomSslConfiguration {
+object Main extends App {
   implicit val system = ActorSystem("api-system")
   val api = system.actorOf(Props[Api], "api")
-  val settings = ServerSettings(system)
 
   IO(Http) ! Http.Bind(api, "0.0.0.0", port = 8080)
-  IO(Http) ! Http.Bind(api, "0.0.0.0", port = 8443, settings = Some(settings.copy(sslEncryption = true)))
 }
