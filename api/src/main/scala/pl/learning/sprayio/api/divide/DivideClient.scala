2702040b793f185d1cbc09f7f00a2db49513feaf
kmichal <michal.kowol@gmail.com>
2014-12-01 23:09:08 +0100

    NT Added LoggingReceive

--- api/src/main/scala/pl/learning/sprayio/api/divide/DivideClient.scala
+++ api/src/main/scala/pl/learning/sprayio/api/divide/DivideClient.scala
@@ -1,13 +1,15 @@
 package pl.learning.sprayio.api.divide
 
 import akka.actor.Actor
+import akka.actor.Status.Failure
+import akka.event.LoggingReceive
 import pl.learning.sprayio.api.NotifyOnError
 
 class DivideClient extends Actor {
 
   var successCount = 0
 
-  override def receive = {
+  override def receive = LoggingReceive {
     case DivideNumbers(a, b) => NotifyOnError {
       successCount += 1
       sender ! DivideResult(a / b, successCount)
