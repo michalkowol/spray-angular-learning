c60e6e673c5a0c2b02f9792a38d02dbdf743cfac
kmichal <michal.kowol@gmail.com>
2014-12-01 23:13:04 +0100

    NT Added alternative implementation of DivideClient - with preRestart

--- api/src/main/scala/pl/learning/sprayio/api/divide/DivideClient2.scala
+++ api/src/main/scala/pl/learning/sprayio/api/divide/DivideClient2.scala
@@ -5,14 +5,18 @@ import akka.actor.Status.Failure
 import akka.event.LoggingReceive
 import pl.learning.sprayio.api.NotifyOnError
 
-class DivideClient extends Actor {
+class DivideClient2 extends Actor {
 
   var successCount = 0
 
   override def receive = LoggingReceive {
-    case DivideNumbers(a, b) => NotifyOnError {
+    case DivideNumbers(a, b) =>
       successCount += 1
       sender() ! DivideResult(a / b, successCount)
-    }
+  }
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    sender() ! Failure(reason)
+    super.preRestart(reason, message)
   }
 }
