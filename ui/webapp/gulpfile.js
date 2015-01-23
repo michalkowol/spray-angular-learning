64da217df5c6a03ae8bd3e431150560cbf2faf30
kmichal <michal.kowol@gmail.com>
2015-01-23 01:10:56 +0100

    NT Downgrade gulp-usemin

--- ui/webapp/gulpfile.js
+++ ui/webapp/gulpfile.js
@@ -68,8 +68,7 @@
     gulp.task('usemin', function () {
         return gulp.src('dist/**/*.html')
             .pipe(usemin({
-                //css: [minifycss(), 'concat', prefix()],
-                css: ['concat', prefix()],
+                css: [minifycss(), 'concat', prefix()],
                 js: [uglify()]
             }))
             .pipe(gulp.dest('dist'));
